6aaa0e1688787deccf2835cabb7beb4613451ce1
Eb <ibbad@users.noreply.github.com>
2015-12-07 02:30:37 +0200

    Update questions-python.py

--- Questions/questions-python.py
+++ Questions/questions-python.py
@@ -1,10 +1,7 @@
 def title_case(title, minor_words=""):
-    title = title.upper()
-    minor_words = minor_words.upper()
-
     capt= " ".join([(word[0].upper()+word[1:].lower())
-                      if word not in minor_words.split(" ") else (word.lower())
-                      for word in title.split(" ")])
+                      if word not in minor_words.lower().split(" ") else (word.lower())
+                      for word in title.lower().split(" ")])
     return capt[0].upper()+capt[1:]
     
 print title_case('a clash of KINGS', 'a an the of') # should return: 'A Clash of Kings'
