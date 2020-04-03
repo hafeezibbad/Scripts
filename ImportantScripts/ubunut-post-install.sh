189b28134f16e597fabc4021dd14b7cb598f2055
Eb <ibbad@users.noreply.github.com>
2020-04-03 21:31:42 +0300

    Update ubunut-post-install.sh

--- ImportantScripts/ubunut-post-install.sh
+++ ImportantScripts/ubunut-post-install.sh
@@ -1,50 +1,62 @@
 #!/bin/bash
-# post installing script for Ubuntu 14.04 Trusty Tahr Desktop
-#
-# Check for Updates
-sudo apt-get -y update
-# Upgrade Packages
-sudo apt-get -y upgrade
-# Major Upgrades
-sudo apt-get -y dist-upgrade
-# Install Essentials
-sudo apt-get -y install dconf-editor nitrogen synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras bleachbit flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras xbmc cheese filezilla chromium-browser vim ufw tree inkscape gparted gimp shutter remmina putty remmina-plugin-* clamav byobu tcpdump gparted htop traceroute mtr netcat socat htop traceroute mtr netcat socat iftop tshark iperf nmap tcputils cryptsetup lynis rkhunter chkrootkit multitail rdiff-backup duplicity nethogs iotop nethogs iotop screen tmux apt-file inxi makepasswd iptraf curl hddtemp dtrx python-pip build-essential python-dev aptitude git ethtool mtr-tiny pv grc mosh aria2 htop hping3
-sudo apt-get install xpad
-sudo apt-get -y install python-virtualenv python-setuptools python-dev build-essential python-matplotlib
-sudo pip install --upgrade virtualenv
-sudo apt-get install -y python-pip
-sudo apt-get -y install git
-sudo pip -y install Glances
-sudo pip -y install PySensors
-# Install oracle Java 8
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java7-installer
-# Play encrypted DVD in Ubuntu 14.04
-sudo apt-get -y install libdvdread4
-sudo /usr/share/doc/libdvdread4/install-css.sh
+# Post-install script for ubuntu installation
+
+# Check for update
+sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
+
+# Install essential utilities
+sudo apt install -y git vlc flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras xbmc cheese filezilla chromium-browser vim ufw tree inkscape gparted gimp shutter remmina putty remmina-plugin-* clamav byobu tcpdump gparted htop traceroute mtr netcat socat htop traceroute mtr netcat socat iftop tshark iperf nmap tcputils cryptsetup lynis rkhunter chkrootkit multitail rdiff-backup duplicity nethogs iotop nethogs iotop screen tmux apt-file inxi makepasswd iptraf curl hddtemp dtrx python-pip build-essential python-dev aptitude git ethtool mtr-tiny pv grc mosh aria2 htop hping3
+
 # Disable shopping suggestions
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
-# Chromium Flash
-sudo apt-get -y install pepperflashplugin-nonfree
-sudo update-pepperflashplugin-nonfree --install
+
 # crash report popups, disable apport
 sudo sed -i 's/1/0/g' /etc/default/apport
 sudo service apport stop
-# Install Handbrake with MP4
-sudo apt-get purge handbrake # remove any old versions
-sudo add-apt-repository ppa:stebbins/handbrake-snapshots
-sudo apt-get -y update
-sudo apt-get -y install handbrake-gtk
-# Enable Firewall
-sudo ufw enable
+
+# Install python and related dependencies
+sudo apt -y install python3-setuptools python3-dev python3-venv build-essential python-matplotlib python3-pip
+
+# Major Upgrades
+sudo apt-get -y dist-upgrade
+
+# Font-powerline 
+sudo apt install fonts-powerline  
+
+# Install brave browser
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com `lsb_release -sc` main" >> /etc/apt/sources.list.d/brave.list'
+sudo apt update
+sudo apt install brave-browser brave-keyring
+
+# Install Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# Install bat
+# https://github.com/sharkdp/bat#installation
+
+# Useful utilities
+# https://github.com/MrRaindrop/tree-cli
+# https://httpie.org/
+# https://hisham.hm/htop/
+# https://linuxize.com/post/linux-watch-command/
+# https://lnav.org/
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+sudo apt install -y tree httpie watch lnav kubectl
+
+# Color scheme for ubuntu https://github.com/Mayccoll/Gogh/blob/master/content/install.md
+
 # Update DB (locate)
 sudo updatedb
+
+sudo apt -y update && sudo apt -y upgrade
 # Clean Up
 echo "Cleaning Up" &&
 sudo apt-get -y -f install &&
 sudo apt-get -y autoremove &&
 sudo apt-get -y autoclean &&
 sudo apt-get -y clean
-#
-sudo apt-get -y update && sudo apt-get -y upgrade
